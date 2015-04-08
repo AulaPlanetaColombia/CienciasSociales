c1c518ac55b340ed36b0f5de3f840e2509c61dfd
Marcela Guevara B <marsguev@hotmail.com>
2015-04-07 21:32:56 -0500

    CS_06_04_CO, CS_06_02_CO y CS_04_02_CO editados.
    De todo un poco... Mi trabajo de una semana...

---
+++ fuentes/contenidos/grado04/guion02/CS_04_02_REC200.mm
@@ -0,0 +1,108 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ccccff" COLOR="#ff00cc" CREATED="1428111926821" ID="ID_1354310239" MODIFIED="1428255023754" TEXT="Descubrimiento de Am&#xe9;rica">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1428112021588" ID="ID_1546544589" MODIFIED="1428114116216" POSITION="right" STYLE="bubble" TEXT="Causas">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1428112132547" ID="ID_1641413989" MODIFIED="1428254614259" TEXT="Necesidad de nuevos mercados de la burgues&#xed;a comercial">
+<node COLOR="#ff3333" CREATED="1428112420618" ID="ID_357597070" MODIFIED="1428112525082" STYLE="bubble" TEXT="Debido a">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428112537729" ID="ID_559174131" MODIFIED="1428254727437" TEXT="Aumento del comercio en Europa"/>
+<node CREATED="1428112555284" ID="ID_832815137" MODIFIED="1428254731316" TEXT="Bloqueo de rutas terrestres por turcos"/>
+<node CREATED="1428112574054" ID="ID_960189358" MODIFIED="1428254734629" TEXT="Necesidad de nuevas rutas mar&#xed;timas para comerciar con Oriente"/>
+</node>
+</node>
+<node CREATED="1428112171877" ID="ID_1197153111" MODIFIED="1428254629216" TEXT="Alianza entre los Reyes Cat&#xf3;licos y la burgues&#xed;a comercial">
+<node COLOR="#ff3333" CREATED="1428112607689" ID="ID_1475397668" MODIFIED="1428112639721" TEXT="Con el objetivo de">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428112647016" ID="ID_1441276569" MODIFIED="1428254737160" TEXT="Controlar el poder de la nobleza"/>
+<node CREATED="1428112666551" ID="ID_386747480" MODIFIED="1428254739816" TEXT="Imponer su poder y consolidar los territorios de Espa&#xf1;a"/>
+</node>
+</node>
+<node CREATED="1428112224961" ID="ID_847985245" MODIFIED="1428254663664" TEXT="Inter&#xe9;s de los reyes por expandir sus territorios y riquezas">
+<node COLOR="#ff3333" CREATED="1428112733642" ID="ID_348960427" MODIFIED="1428112748947" TEXT="Para">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428112754175" ID="ID_145951782" MODIFIED="1428254779749" TEXT="Ser potencia en Europa"/>
+<node CREATED="1428113124660" ID="ID_949675382" MODIFIED="1428254783760" TEXT="Forjar armas para la guerra"/>
+<node CREATED="1428113141786" ID="ID_1444173595" MODIFIED="1428254787272" TEXT="Acu&#xf1;ar monedas para pagar a los ej&#xe9;rcitos"/>
+</node>
+</node>
+<node CREATED="1428112253999" ID="ID_170844907" MODIFIED="1428254709105" TEXT="Inter&#xe9;s de monarqu&#xed;a e Iglesia por difundir el cristianismo">
+<node COLOR="#ff3333" CREATED="1428112813623" ID="ID_1110966063" MODIFIED="1428112829274" TEXT="Con el fin de">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428112833031" ID="ID_1696999861" MODIFIED="1428254790116" TEXT="Ganar la guerra contra los infieles"/>
+<node CREATED="1428112851297" ID="ID_98045691" MODIFIED="1428254795335" TEXT="Imponer el cristianismo en Europa y el resto del mundo"/>
+</node>
+</node>
+<node CREATED="1428112300651" ID="ID_672128928" MODIFIED="1428254646320" TEXT="Avances cient&#xed;ficos y t&#xe9;cnicos que permitieron la navegaci&#xf3;n">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff3333" CREATED="1428112903357" ID="ID_65823065" MODIFIED="1428112989783" TEXT="Como">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428112924520" ID="ID_491386923" MODIFIED="1428254928993" TEXT="P&#xf3;lvora, imprenta, astrolabio, cuadrante, br&#xfa;jula, portulanos, almanaques y embarcaciones (carabelas y naos)"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1428113014302" ID="ID_293344207" MODIFIED="1428114119363" POSITION="left" STYLE="bubble" TEXT="Exploraciones de Espa&#xf1;a y Portugal">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1428113441723" ID="ID_1034332700" MODIFIED="1428113449691" TEXT="Portugal">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff3333" CREATED="1428113479876" ID="ID_512411617" MODIFIED="1428113507647" TEXT="Condiciones propicias">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428113519720" ID="ID_1683208036" MODIFIED="1428254189143" TEXT="Pen&#xed;nsula Ib&#xe9;rica"/>
+<node CREATED="1428113531248" ID="ID_645075095" MODIFIED="1428254230935" TEXT="Porci&#xf3;n mar&#xed;tima"/>
+<node CREATED="1428113541298" ID="ID_772347299" MODIFIED="1428254216202" TEXT="Comercio mar&#xed;timo"/>
+<node CREATED="1428113553195" ID="ID_933033524" MODIFIED="1428254223790" TEXT="Financiamiento de la monarqu&#xed;a"/>
+</node>
+<node COLOR="#ff3333" CREATED="1428113569483" ID="ID_544041707" MODIFIED="1428113590747" TEXT="Territorios descubiertos">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428113599576" ID="ID_1602631711" MODIFIED="1428254354434" TEXT="Costas e islas de &#xc1;frica, India y Ocean&#xed;a"/>
+</node>
+</node>
+<node CREATED="1428113453370" ID="ID_1169692868" MODIFIED="1428113471204" TEXT="Espa&#xf1;a">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff3333" CREATED="1428113699642" ID="ID_1944713268" MODIFIED="1428113713088" TEXT="Condiciones propicias">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428113716139" ID="ID_1347358114" MODIFIED="1428254253918" TEXT="Pen&#xed;nsula Ib&#xe9;rica"/>
+<node CREATED="1428113728204" ID="ID_1699455209" MODIFIED="1428254259215" TEXT="Porci&#xf3;n mar&#xed;tima"/>
+<node CREATED="1428113743232" ID="ID_173840207" MODIFIED="1428254408050" TEXT="Comercio mar&#xed;timo"/>
+<node CREATED="1428113754732" ID="ID_1727080135" MODIFIED="1428254420977" TEXT="Financiamiento de los Reyes Cat&#xf3;licos"/>
+</node>
+<node COLOR="#ff3333" CREATED="1428113774311" ID="ID_1209424776" MODIFIED="1428113815687" TEXT="Territorios descubiertos">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428113800857" ID="ID_841326299" MODIFIED="1428254496018" TEXT="Costas e islas de Centro y Suram&#xe9;rica en el Oc&#xe9;ano Atl&#xe1;ntico"/>
+</node>
+<node COLOR="#ff3333" CREATED="1428113853275" ID="ID_235458970" MODIFIED="1428113922449" TEXT="Viajes de descubrimiento">
+<font NAME="SansSerif" SIZE="11"/>
+<node CREATED="1428113905757" ID="ID_350399897" MODIFIED="1428113911115" TEXT="Cuatro viajes">
+<node CREATED="1428113913933" ID="ID_1121780064" MODIFIED="1428113919083" TEXT="Crist&#xf3;bal Col&#xf3;n"/>
+</node>
+<node CREATED="1428113922449" ID="ID_1276753129" MODIFIED="1428113926440" TEXT="Viajes menores">
+<node CREATED="1428113928945" ID="ID_776749923" MODIFIED="1428254982615" TEXT="Alonso de Ojeda, Juan de la Cosa, Am&#xe9;rico Vespucio, Alonso Ni&#xf1;o, Crist&#xf3;bal Guerra, Vicente Y&#xe1;&#xf1;ez, Rodrigo de Bastidas"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1428114087601" ID="ID_220286401" MODIFIED="1428114122297" POSITION="right" STYLE="bubble" TEXT="Consecuencias">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1428114155657" ID="ID_1769343636" MODIFIED="1428114165223" TEXT="Para Europa">
+<node CREATED="1428114206821" ID="ID_690248068" MODIFIED="1428255050418" TEXT="Ampliaci&#xf3;n de la visi&#xf3;n de mundo"/>
+<node CREATED="1428114244368" ID="ID_1421850064" MODIFIED="1428255054503" TEXT="Revoluci&#xf3;n de conocimientos en Geograf&#xed;a y Astronom&#xed;a"/>
+<node CREATED="1428114283631" ID="ID_630865089" MODIFIED="1428255057846" TEXT="Complemento de mapas cartogr&#xe1;ficos"/>
+<node CREATED="1428114314556" ID="ID_18922361" MODIFIED="1428255066896" TEXT="Espa&#xf1;a y Portugal, potencias de Europa"/>
+<node CREATED="1428114431437" ID="ID_1669866531" MODIFIED="1428255107519" TEXT="Inicio del colonialismo"/>
+<node CREATED="1428114338014" ID="ID_1294115436" MODIFIED="1428255135578" TEXT="Capitalismo comercial por el Oc&#xe9;ano Atl&#xe1;ntico"/>
+<node CREATED="1428114368506" ID="ID_442443311" MODIFIED="1428255139703" TEXT="Proliferaci&#xf3;n de viajes menores de descubrimiento"/>
+<node CREATED="1428114382104" ID="ID_947949457" MODIFIED="1428255146826" TEXT="Difusi&#xf3;n del cristianismo en Europa y sus colonias"/>
+</node>
+<node CREATED="1428114167600" ID="ID_1268694671" MODIFIED="1428114172140" TEXT="Para Am&#xe9;rica">
+<node CREATED="1428114403941" ID="ID_1093399300" MODIFIED="1428255150844" TEXT="Inclusi&#xf3;n dentro del mapa mundial"/>
+<node CREATED="1428114446245" ID="ID_1571288963" MODIFIED="1428255153438" TEXT="Colonias espa&#xf1;olas"/>
+<node CREATED="1428114487472" ID="ID_331705386" MODIFIED="1428255156532" TEXT="P&#xe9;rdida de libertad, identidad, cultura y costumbres"/>
+<node CREATED="1428114508052" ID="ID_172198898" MODIFIED="1428255262158" TEXT="Fin del per&#xed;odo Ind&#xed;gena"/>
+</node>
+</node>
+</node>
+</map>
