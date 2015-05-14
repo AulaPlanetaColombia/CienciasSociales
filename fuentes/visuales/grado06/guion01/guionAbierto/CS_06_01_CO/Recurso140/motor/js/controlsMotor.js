e372607187e8cd9150ac87932cf44572afb46933
Jeysson Moreno <yeyemoto@hotmail.com>
2015-05-14 16:50:31 -0500

    14/05 - 1

---
+++ fuentes/visuales/grado06/guion01/guionAbierto/CS_06_01_CO/Recurso140/motor/js/controlsMotor.js
@@ -0,0 +1,234 @@
+function BaseResposta(_index, _width, _height, _radius) {
+  
+  this.resposta  = null;
+  this.index = _index;
+  this.width = _width;
+  this.height = _height;
+  this.radius = _radius;
+  this.pregunta =null;
+  
+  this.base = new createjs.Shape();
+  
+  this.contenedor = new createjs.Container();
+  this.dibuixa();
+}
+
+BaseResposta.prototype.setReposta = function(_resposta){
+  this.resposta = _resposta;
+  this.resposta.index = this.index;
+  this.resposta.base = this;
+  //this.contenedor.addChild( this.resposta.contenedor );
+}
+
+BaseResposta.prototype.dibuixa = function() {
+  
+  this.base.graphics.beginFill("#fde8c2").drawRoundRect(0, 0, this.width, this.height, this.radius);
+  this.base.graphics.beginStroke("#f8B334").drawRoundRect(0, 0, this.width, this.height, this.radius);
+  this.base.mouseEnabled = false;
+  this.contenedor.addChild( this.base );
+  
+}
+
+function Resposta(_texte, _width, _height, _radius) {
+	this.width = _width;
+  	this.height = _height;
+  	this.radius = _radius;
+  	this.texte = _texte;
+  	this.index = 0;
+  	this.idResposta = -1;
+  	this.idPregunta = -1; 
+  	this.ordre = -2;
+  	this.error = false;
+  	this.base = null;
+  	
+  	this.correccio = new createjs.Shape();
+  	
+  	this.fons = new createjs.Shape();	
+	this.text = new createjs.RichText();	
+	this.contenedor = new createjs.Container();
+  	this.dibuixa();
+}
+
+Resposta.prototype.lightResponse = function(){
+	this.fons.graphics.clear();
+	this.fons.graphics.beginFill("#f8B334").drawRoundRect(0, 0, this.width, this.height, this.radius);
+	this.fons.shadow = new createjs.Shadow("#777", 4, 4, 10);
+}
+Resposta.prototype.unlightResponse = function(){
+	this.fons.graphics.beginFill("#fff").drawRoundRect(0, 0, this.width, this.height, this.radius);
+	this.fons.shadow = null;
+}
+Resposta.prototype.erronia = function(){
+	this.error = true;
+	this.correccio.graphics.beginStroke("#E1001A").setStrokeStyle(3).drawRoundRect(-2, -2, this.width + 4, this.height + 4, this.radius);
+	this.contenedor.addChild( this.correccio );
+}
+Resposta.prototype.correcte = function(){
+	this.error = false
+	this.correccio.graphics.beginStroke("#41A62A").setStrokeStyle(3).drawRoundRect(-2, -2, this.width + 4, this.height + 4, this.radius);
+	this.contenedor.addChild( this.correccio );
+}
+Resposta.prototype.removeError = function(){
+	if(this.error) this.contenedor.removeChild( this.correccio );
+}
+Resposta.prototype.dibuixa = function() {
+	this.fons.graphics.beginFill("#fff").drawRoundRect(0, 0, this.width, this.height, this.radius);
+	
+	this.text.font = (Contenedor.datosXML.plataforma.grado == 1)? "16px Arial" : "14px Arial" ;
+	this.text.fontSize = (Contenedor.datosXML.plataforma.grado == 1)? 16 : 14 ;
+	this.text.color = "#0D3158";
+	this.text.text = this.texte ;
+	this.text.x = 8;
+	this.text.y = 3 ;
+	this.text.lineWidth = 190;
+	this.text.lineHeight = 22;
+	this.text.mouseEnabled = false;
+	
+	this.contenedor.addChild( this.fons );
+	this.contenedor.addChild( this.text );
+}
+
+
+function Pregunta(i, pregunta, numcat){
+  	this.texte = pregunta.text;
+  	this.index = i;
+  	this.id = pregunta.id;
+  	this.contenedor = new createjs.Container();
+  	this.contenedor.y = Motor.INITIAL_Y + (this.index % 3) * 130+25;
+  	this.contenedor.x =  Math.floor(this.index / 3) * 330 + 25;
+  	
+  	//fons
+  	this.fons = new createjs.Shape();
+  	this.fons.graphics.beginFill("#fde8c2").drawRoundRect(0, 0, 325, 125, 5);
+  	
+  	//titol	
+	this.text = new createjs.RichText();	
+	this.text.font = (Contenedor.datosXML.plataforma.grado == 1)? "16px Arial" : "14px Arial" ;
+	this.text.fontSize = (Contenedor.datosXML.plataforma.grado == 1)? 16 : 14 ;
+	this.text.color = "#0D3158";
+	this.text.text = this.texte;
+	this.text.y = 4 ;
+	this.text.x = 120;
+	this.text.lineWidth = 200;
+	this.text.lineHeight = 20;
+	this.text.mouseEnabled = false;
+	
+	//mascara de cantonades arrodonides
+	this.text.mask = new createjs.Shape();
+	this.text.mask.graphics.beginFill("#fff").drawRoundRect(0, 0, 200, 40, 1);
+	this.text.mask.x =120;
+	this.text.mask.y = 4;
+	
+	this.imagen = new Imagen(pregunta);
+	// contenedors
+	this.contenedors = new Array();
+	
+	this.contenedor.addChild( this.fons );
+	this.contenedor.addChild( this.text );
+	this.contenedor.addChild( this.imagen.contenedor );
+	
+	for(var i=1; i <= pregunta.respuestas.length; i++)
+	{
+		var inici = new BaseResposta(i,Motor.RESP_WIDTH, Motor.RESP_HEIGHT , 5);
+		inici.contenedor.x = 120;
+        inici.contenedor.y = 20 + 34 * i;
+        
+		inici.pregunta = this;
+		this.contenedor.addChild( inici.contenedor );
+		this.contenedors[i-1] = inici;
+	}
+	this.currentIndex = 0;
+}
+Pregunta.prototype.addElement = function(element, delay ) {
+	
+	//if(this.currentIndex < this.contenedors.length)
+	//{
+	for( var key in this.contenedors)
+	{
+		if( this.contenedors[key].resposta == null){
+			//this.contenedors[this.currentIndex].pregunta = this;
+			Motor.contenedor.setChildIndex ( element.contenedor,  Motor.contenedor.getNumChildren ()  );
+			// coordenades del element desti
+			var xfi = this.contenedor.x + this.contenedors[key].contenedor.x + 1;
+			var yfi = this.contenedor.y + this.contenedors[key].contenedor.y + 1;
+	
+			// coloquem l'element origen a la casella de l'element destí
+			this.contenedors[key].setReposta(element); 		 
+			createjs.Tween.get(element.contenedor).wait(delay).to({x: xfi, y: yfi}, 250, createjs.Ease.circOut);
+
+			return true;
+		}
+	}
+	return false;
+
+}
+Pregunta.prototype.situar = function(element, delay ) {
+	
+	for( var key in this.contenedors)
+	{
+		if( this.contenedors[key].resposta == null){
+			//this.contenedors[this.currentIndex].pregunta = this;
+			Motor.contenedor.setChildIndex ( element.contenedor,  Motor.contenedor.getNumChildren ()  );
+			// coordenades del element desti
+			var xfi = this.contenedor.x + this.contenedors[key].contenedor.x + 1;
+			var yfi = this.contenedor.y + this.contenedors[key].contenedor.y + 1;
+	
+			// coloquem l'element origen a la casella de l'element destí
+			this.contenedors[key].setReposta(element); 
+			
+			element.contenedor.x = xfi;
+			element.contenedor.y = yfi;		 
+			//createjs.Tween.get(element.contenedor).wait(delay).to({x: xfi, y: yfi}, 250, createjs.Ease.circOut);
+	
+			return true;
+		}
+	}
+	return false;
+
+}
+Pregunta.prototype.removeElement = function(element, resituar ) {
+	
+	var key="";
+	var trobat = false
+	for( key in this.contenedors )
+	{
+		if( this.contenedors[key].resposta == element )
+		{
+			trobat= true;
+			break;
+		}
+	}
+	
+	if(trobat)
+	{
+		this.contenedors[key].resposta = null;
+	}
+}
+
+function Imagen( pregunta ){	    
+	    // get imagen
+    this.contenedor = new createjs.Container();
+
+    var img = new Image();
+    var objeto = this;
+	img.onload = function () {
+	   	objeto.imagen = new createjs.Bitmap(this);
+
+ 		objeto.imagen.x = 0;
+		objeto.imagen.y = 5;
+		//escalem imatges
+		objeto.imagen.scaleX =  115 / this.width;
+		objeto.imagen.scaleY =  115 / this.height;
+
+		//mascara de cantonades arrodonides
+		objeto.imagen.mask = new createjs.Shape();
+		objeto.imagen.mask.graphics.beginFill("#fff").drawRoundRect(0, 0, 115, 115, 4);
+		objeto.imagen.mask.x = 0;
+	 	objeto.imagen.mask.y = 5;
+		
+		objeto.contenedor.addChild( objeto.imagen);
+	};
+	
+	if( pregunta.imagen!= "")
+		img.src = Motor.IMG + pregunta.imagen;
+}
---
+++ fuentes/visuales/grado06/guion01/guionAbierto/CS_06_01_CO/Recurso190/motor/js/lang.js
@@ -0,0 +1,217 @@
+//***************************************************************
+// Variables y funciones sobre idiomas
+//***************************************************************
+
+var LangRes = {
+
+	// Idiomes disponibles
+	LANG_ID_ES		: 0,
+	LANG_ID_EN		: 1,
+	LANG_ID_VAL		: 2, // El idioma valenciano no esta reconocido
+	LANG_ID_GAL		: 3,
+	LANG_ID_CAT		: 4,
+
+	// Indicadors del recurs
+	ENTREGA_EN_MANO		:	0,
+	ENVIAR			:	1,
+	ADJUNTAR		:	2,
+	GUARDAR			:	3,
+	CANCELAR		:	4,
+	CONTINUAR		:	5,
+	CONFIRMAR_SIN_RESP	:	6,
+	NO_HAY_ARCH_SUBIDOS	:	7,
+	CORREGIR		:	8,
+	DESCARGAR_ARCH		:	9,
+	DESCARGAR_ARCH_TIT	:	10,
+	CERRAR			:	11,
+	CARGANDO_ACT		:	12,
+	ACTIVITY_DONE		:	13,
+	BROWSE_MODE		:	14,
+	ACTIVITY_SENT		:	15,
+	ACTIVITY_VIEWED		:	16,
+	ACTIVITY_NOT_VIEWED	:	17,
+	HTML_PAGE_TITLE		:	18,
+	
+	// Text codificat que indica l'idioma, equivalent al langId però que
+	// es fa servir als XML
+	langNames: [
+		{ id: "es", nameId: 0 },
+		{ id: "en", nameId: 0 },
+		{ id: "val", nameId: 0 },
+		{ id: "gal", nameId: 0 },
+		{ id: "cat", nameId: 0 }
+	],
+
+
+	// Array de traduccions
+	langTexts:
+	[
+		[ // es
+			"Entrega el trabajo en mano o por email.",
+			"Enviar",
+			"Adjuntar trabajo",
+			"Guardar",
+			"Cancelar",
+			"Continuar",
+			"Hay preguntas sin responder. Si continúas, se enviará al profesor el ejercicio para evaluar. Si quieres hacer alguna modificación, haz clic en Cancelar.",
+			"No hay archivos subidos aún.",
+			"Corregir",
+			"Descargar Archivo",
+			"Archivo de la actividad",
+			"Cerrar",
+			"Cargando actividad...",
+			"Ya has realizado esta actividad antes.",
+			"Esta actividad debe asignarse como tarea para poderse realizar, o bien entregarse en mano o por email.",
+			"La actividad ya ha sido enviada al profesor.",
+			"El alumno ha abierto la actividad.",
+			"Actividad aún por empezar.",
+			"Actividad"
+		],
+		[ // en
+			"Hand in to your teacher directly or by email.",
+			"Send",
+			"Add file",
+			"Save",
+			"Cancel",
+			"Next",
+			"You still have some questions to answer. Press SEND for your teacher to mark them. Press CANCEL if you want to change or add anything.",
+			"There are no uploaded files.",
+			"Correct",
+			"Download file",
+			"Download activity file",
+			"Close",
+			"Loading activity...",
+			"This activity has been completed before.",
+			"To answer here, the activity should be assigned as a task before (Asignar tarea). If necessary, hand in to your teacher directly or by email.",
+			"The activity has already been sent to your teacher.",
+			"The student has openned the exercise.",
+			"Activity still not started.",
+			"Activity"
+		],
+		[ // valenciano
+			"Lliura el treball en mà o per correu electrònic.",
+			"Envia",
+			"Adjunta el treball",
+			"Guarda",
+			"Cancel·la",
+			"Continua",
+			"Hi ha preguntes sense contestar. Si continues, s'enviarà al professor l'exercici per avaluar. Si vols fer alguna modificació, fes clic en Cancel·la.",
+			"Encara no s'ha carregat cap fitxer.",
+			"Corregeix",
+			"Descarrega el fitxer",
+			"Fitxer de l'activitat",
+			"Tanca",
+			"S'està carregant l'activitat...",
+			"Ja has fet aquesta activitat.",
+			"Aquesta activitat ha d'assignar-se com a tasca per a poder fer-la, o bé lliurar-la en mà o per correu electrònic.",
+			"L'activitat ja s'ha enviat al professor.",
+			"L'alumne ha obert l'activitat.",
+			"Activitat no iniciada encara.",
+			"Activitat"
+		],
+		[ // gallego
+			"Entrega o traballo en man ou por correo electrónico.",
+			"Enviar",
+			"Anexar traballo",
+			"Gardar",
+			"Cancelar",
+			"Continuar",
+			"Hai preguntas sen responder. Se continúas, enviarase ao profesor o exercicio para avaliar. Se queres facer algunha modificación, fai clic en Cancelar.",
+			"Non se subiron arquivos aínda.",
+			"Corrixir",
+			"Descargar arquivo",
+			"Arquivo da actividade",
+			"Pechar",
+			"Cargando actividade...",
+			"Xa realizaches esta actividade antes.",
+			"Esta actividade debe asignarse como tarefa para que se poida realizar, ou ben entregarse en man ou por correo electrónico.",
+			"Xa se enviou a actividade ao profesor.",
+			"O alumno abriu a actividade.",
+			"Actividade aínda por empezar.",
+			"Actividade"
+		],
+		[ // catalán
+			"Entrega el treball en mà o per correu electrònic.",
+			"Envia",
+			"Adjunta el treball",
+			"Guarda",
+			"Cancel·la",
+			"Continua",
+			"Hi ha preguntes sense contestar. Si continues, s'enviarà al professor l'exercici per avaluar. Si vols fer alguna modificació, fes clic a Cancel·la.",
+			"Encara no s'ha carregat cap fitxer.",
+			"Corregeix",
+			"Descarrega el fitxer",
+			"Fitxer de l'activitat",
+			"Tanca",
+			"S'està carregant l'activitat...",
+			"Ja has fet aquesta activitat.",
+			"Aquesta activitat s'ha d'assignar com a tasca per poder fer-la, o bé entregar-la en mà o per correu electrònic.",
+			"L'activitat ja s'ha enviat al professor.",
+			"L'alumne ha obert l'activitat.",
+			"Activitat no iniciada encara.",
+			"Activitat"
+		]
+	],
+
+
+	curLang: null,
+	
+	setLangId: function (langId) {
+		// Comprovem que estigui dins del marge de l'array de traduccions
+		// Si no és el cas, apliquem sempre l'espanyol
+		if (langId >= 0 && langId < this.langTexts.length) {
+			this.curLang = this.langTexts[langId];
+		} else {
+			this.curLang = this.langTexts[LangRes.LANG_ID_ES];
+		}
+	},
+	
+	getLangId: function() {
+		for (var i=0;i<this.langTexts.length;i++) {
+			if (this.langTexts[i] == this.curLang)
+				return i;
+		}
+		return LangRes.LANG_ID_ES;
+	},
+		
+	getLangIdFromString: function(lang) {
+		/*
+		if (lang == "en")
+			return LangRes.LANG_ID_EN;
+		else if (lang == "gal")
+			return LangRes.LANG_ID_GAL;
+		else if (lang == "val")
+			return LangRes.LANG_ID_VAL;
+		else
+			return LangRes.LANG_ID_ES;
+		*/
+	       for (var i=0;i<LangRes.langNames.length;i++) {
+		       if (lang == LangRes.langNames[i].id)
+			       return i;
+	       }
+	       return LangRes.LANG_ID_ES;
+	},
+	
+	getLangIdAsString: function() {
+		var langId = this.getLangId();
+		return LangRes.langNames[langId].id;
+		/*
+		var langId = this.getLangId();
+		if (langId == LangRes.LANG_ID_EN)
+			return "en";
+		else if (langId == LangRes.LANG_ID_VAL)
+			return "val";
+		else if (langId == LangRes.LANG_ID_GAL)
+			return "gal";
+		else
+			return "es";
+		*/
+	}
+	
+};
+
+// COM CRIDAR
+// Per posar l'idioma
+// LangRes.setLangId(LangRes.LANG_ID_ES);
+// Per recurperar un text
+// var text = LangRes.curLang[LangRes.MSG_EXERCISE_SAVED];
