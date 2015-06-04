3cbc39928600ab62aea02b18e9edb1e5ddd18f01
Marcela Guevara B <marsguev@hotmail.com>
2015-06-04 17:43:23 -0500

    De todo un poco
    Avances provisionales

---
+++ fuentes/contenidos/grado04/guion10/CS_04_10_REC110.mm
@@ -0,0 +1,133 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ccff66" CREATED="1433222786419" ID="ID_165053875" MODIFIED="1433226431918">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Los Derechos Humanos de los Colombianos</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1433223292486" ID="ID_487106476" MODIFIED="1433224603688" POSITION="right" TEXT="los consagra la" VSHIFT="-7">
+<node BACKGROUND_COLOR="#ffff66" CREATED="1433223308244" HGAP="17" ID="ID_1190769801" MODIFIED="1433225407479" STYLE="bubble" TEXT="Constituci&#xf3;n Pol&#xed;tica" VSHIFT="11">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433223346392" HGAP="14" ID="ID_807749706" MODIFIED="1433224617187" STYLE="fork" TEXT="se clasifican en Derechos de" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ccff" CREATED="1433223389944" ID="ID_650554066" MODIFIED="1433225470456" STYLE="bubble" TEXT="Primera generaci&#xf3;n">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433223427346" HGAP="14" ID="ID_225667846" MODIFIED="1433225475727" STYLE="fork" TEXT="son los" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ccff" CREATED="1433223434042" HGAP="10" ID="ID_209909596" MODIFIED="1433225769919" STYLE="bubble" TEXT="Derechos fundamentales" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224111526" HGAP="13" ID="ID_185972843" MODIFIED="1433225778697" STYLE="fork" TEXT="se crean" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ccff" CREATED="1433224121781" ID="ID_511298966" MODIFIED="1433225791392" STYLE="bubble" TEXT="Mecanismos de protecci&#xf3;n">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224259382" HGAP="13" ID="ID_105855512" MODIFIED="1433225828774" STYLE="fork" TEXT="como" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ccff" CREATED="1433224266371" HGAP="18" ID="ID_158464046" MODIFIED="1433225821610" STYLE="bubble" TEXT="La Tutela&#xa;Derecho de petici&#xf3;n&#xa;Habeas corpus&#xa;Habeas data&#xa;Acci&#xf3;n popular&#xa;Acci&#xf3;n de cumplimiento&#xa;Acci&#xf3;n de grupo" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ccff" CREATED="1433224133094" ID="ID_1922084364" MODIFIED="1433225793383" STYLE="bubble" TEXT="Instituciones que protegen los derechos de los colombianos">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224381934" HGAP="13" ID="ID_957968264" MODIFIED="1433225836746" STYLE="fork" TEXT="como" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ccff" CREATED="1433224390931" HGAP="16" ID="ID_1938618505" MODIFIED="1433225842101" STYLE="bubble" TEXT="Procuradur&#xed;a&#xa;Personer&#xed;a&#xa;Defensor&#xed;a del pueblo&#xa;Oficina permanente de Derechos Humanos" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1433223399058" HGAP="18" ID="ID_1674640189" MODIFIED="1433225904636" STYLE="bubble" TEXT="Segunda generaci&#xf3;n" VSHIFT="16">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433223452037" HGAP="13" ID="ID_1701954035" MODIFIED="1433225915927" STYLE="fork" TEXT="son los" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1433223458875" HGAP="14" ID="ID_174378188" MODIFIED="1433225932479" STYLE="bubble" TEXT="Derechos econ&#xf3;micos, sociales y culturales" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224111526" HGAP="15" ID="ID_930900340" MODIFIED="1433225954207" STYLE="fork" TEXT="se crean" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1433224121781" ID="ID_1894179500" MODIFIED="1433225951748" STYLE="bubble" TEXT="Mecanismos de protecci&#xf3;n">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224259382" HGAP="14" ID="ID_1910084468" MODIFIED="1433225981655" STYLE="fork" TEXT="como" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1433224266371" HGAP="11" ID="ID_383349027" MODIFIED="1433225988517" STYLE="bubble" TEXT="La Tutela&#xa;Derecho de petici&#xf3;n&#xa;Habeas corpus&#xa;Habeas data&#xa;Acci&#xf3;n popular&#xa;Acci&#xf3;n de cumplimiento&#xa;Acci&#xf3;n de grupo" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1433224133094" ID="ID_1990453273" MODIFIED="1433225957813" STYLE="bubble" TEXT="Instituciones que protegen los derechos de los colombianos">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224381934" HGAP="13" ID="ID_1016262286" MODIFIED="1433226001733" STYLE="fork" TEXT="como" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#99ff99" CREATED="1433224390931" HGAP="13" ID="ID_1013738361" MODIFIED="1433226014288" STYLE="bubble" TEXT="Procuradur&#xed;a&#xa;Personer&#xed;a&#xa;Defensor&#xed;a del pueblo&#xa;Oficina permanente de Derechos Humanos" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1433223407011" ID="ID_1944859262" MODIFIED="1433226056423" STYLE="bubble" TEXT="Tercera generaci&#xf3;n">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433223499028" HGAP="16" ID="ID_41559142" MODIFIED="1433226069808" STYLE="fork" TEXT="son los" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1433223518674" HGAP="15" ID="ID_1132402321" MODIFIED="1433226079809" STYLE="bubble" TEXT="Derechos de solaridad&#xa;Derechos de los pueblos" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224111526" HGAP="16" ID="ID_825773584" MODIFIED="1433226089086" STYLE="fork" TEXT="se crean" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1433224121781" ID="ID_1461146628" MODIFIED="1433226093567" STYLE="bubble" TEXT="Mecanismos de protecci&#xf3;n">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224259382" HGAP="16" ID="ID_1581275717" MODIFIED="1433226125475" STYLE="fork" TEXT="como" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1433224266371" ID="ID_28733954" MODIFIED="1433226130930" STYLE="bubble" TEXT="La Tutela&#xa;Derecho de petici&#xf3;n&#xa;Habeas corpus&#xa;Habeas data&#xa;Acci&#xf3;n popular&#xa;Acci&#xf3;n de cumplimiento&#xa;Acci&#xf3;n de grupo" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1433224133094" ID="ID_728666344" MODIFIED="1433226104254" STYLE="bubble" TEXT="Instituciones que protegen los derechos de los colombianos">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1433224381934" HGAP="15" ID="ID_1557653548" MODIFIED="1433226145458" STYLE="fork" TEXT="como" VSHIFT="-7">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1433224390931" HGAP="16" ID="ID_1952302456" MODIFIED="1433226158680" STYLE="bubble" TEXT="Procuradur&#xed;a&#xa;Personer&#xed;a&#xa;Defensor&#xed;a del pueblo&#xa;Oficina permanente de Derechos Humanos" VSHIFT="11">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
