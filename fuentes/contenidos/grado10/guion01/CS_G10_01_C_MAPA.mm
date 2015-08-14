b0399ebabd8d9495778728b6c232dd56dfcfd7da
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2015-08-14 16:59:39 -0500

    Mabel Lara: primera edición

---
+++ fuentes/contenidos/grado10/guion01/CS_G10_01_C_MAPA.mm
@@ -0,0 +1,188 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#ff0066" CREATED="1424794367608" ID="ID_1877735738" MODIFIED="1424802134847" TEXT="FIN DE LA GUERRA FR&#xcd;A ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#6699ff" CREATED="1424795894474" ID="ID_1791841340" MODIFIED="1424802695809" POSITION="right" TEXT="LOS CAMBIOS DE GORBACHOV">
+<cloud/>
+<node COLOR="#333300" CREATED="1424796054752" ID="ID_892488398" MODIFIED="1424802476255" TEXT="Gobern&#xf3; la URSS 1985-1991">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424795923795" ID="ID_333871995" MODIFIED="1424802476255" TEXT="Perestroika">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424795995168" ID="ID_973820661" MODIFIED="1424796013164" TEXT="Reforma pol&#xed;ticas">
+<node CREATED="1424796152464" ID="ID_1309057787" MODIFIED="1424796186068" TEXT="Realiza una ser&#xed;e dereformas pol&#xed;ticas. Intyegra l&#xed;ders j&#xf3;venes a la direcci&#xf3;n del Estado."/>
+</node>
+</node>
+<node COLOR="#333300" CREATED="1424795919187" ID="ID_22414781" MODIFIED="1424802476254" TEXT="Glasnot">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424796025127" ID="ID_937242786" MODIFIED="1424796037098" TEXT="Transparencia">
+<node CREATED="1424796134536" ID="ID_1904386771" MODIFIED="1424796148196" TEXT="Da a conocer la situaci&#xf3;n de la sociedad sovi&#xe9;tica"/>
+</node>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424796243440" FOLDED="true" ID="ID_720259128" MODIFIED="1424802820836" POSITION="left" TEXT="CA&#xcd;DA DE LA URSS">
+<cloud COLOR="#cccc00"/>
+<font NAME="SansSerif" SIZE="13"/>
+<node COLOR="#333300" CREATED="1424796323115" ID="ID_1136001824" MODIFIED="1424802368444" TEXT="Junio 1990. Boris Yeltsin lidera la independencia de Federaci&#xf3;n Rusade la URSS">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424796258680" ID="ID_1266169337" MODIFIED="1424802368444" TEXT="Agosto de 1991. Golpe contra Gorbachov">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424796597084" ID="ID_1441885158" MODIFIED="1424802368444" TEXT="Diciembre de 1991. Disoluci&#xf3;n de la URSS">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424796614828" ID="ID_1772896320" MODIFIED="1424802368444" TEXT="Lituania, Estonia, Letonia y Ucrania declaran su independencia">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424796296251" ID="ID_738240016" MODIFIED="1424802368443" TEXT="1991. Disoluci&#xf3;n del Pacto de Varsovia y del CAME">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424796667731" ID="ID_977125222" MODIFIED="1424802368443" TEXT="Diciembre de 1991. Se constituye la Comunidad de Estados Independientes (CEI) ">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424796706435" ID="ID_1924989284" MODIFIED="1424802368443" TEXT="Disoluci&#xf3;n del Partido Comunista Sovi&#xe9;tico">
+<edge COLOR="#ff6699"/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424796870001" ID="ID_682711319" MODIFIED="1424802797318" POSITION="right" TEXT="POTENCIAS DE LA GUERRA FR&#xcd;A">
+<cloud COLOR="#33ff00"/>
+<node COLOR="#333300" CREATED="1424796888129" ID="ID_903072720" MODIFIED="1424802476254" TEXT="Uni&#xf3;n de Rep&#xfa;blicas Socialistas Sovi&#xe9;ticas - URSS">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424797070116" ID="ID_1314716363" MODIFIED="1424797088392" TEXT="Aliados: China, Cuba, pa&#xed;ses de cortina de Hierro"/>
+<node CREATED="1424796958857" ID="ID_225718444" MODIFIED="1424796976582" TEXT="L&#xed;der del bloque comunista"/>
+<node CREATED="1424796977236" ID="ID_1596700796" MODIFIED="1424797120864" TEXT="Potencia entre 1917-1991"/>
+<node CREATED="1424796993732" ID="ID_465846262" MODIFIED="1424797005233" TEXT="Econom&#xed;a planificada"/>
+</node>
+<node COLOR="#333300" CREATED="1424796949016" ID="ID_960275678" MODIFIED="1424802476254" TEXT="Estados Unidos de Am&#xe9;rica">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424797013796" ID="ID_1284349701" MODIFIED="1424797019269" TEXT="L&#xed;der del bloque capitalista"/>
+<node CREATED="1424797019740" ID="ID_1255223151" MODIFIED="1424797037326" TEXT="Se conform&#xf3; como pais en 1776"/>
+<node CREATED="1424797039012" ID="ID_757794857" MODIFIED="1424797046958" TEXT="Econom&#xed;a de mercado"/>
+<node CREATED="1424797053268" ID="ID_77789229" MODIFIED="1424797066852" TEXT="Aliados: pa&#xed;ses de Occidente"/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424799745505" ID="ID_940882460" MODIFIED="1424802820836" POSITION="left" TEXT="MURO DE BERL&#xcd;N">
+<cloud COLOR="#cccc00"/>
+<node CREATED="1424799780242" ID="ID_1982029273" MODIFIED="1424799836293" TEXT="1948 - Los sovi&#xe9;ticos demandan a los aliados retirar sus tropas de Berl&#xed;n"/>
+<node CREATED="1424799963879" ID="ID_1903220992" MODIFIED="1424800072434" TEXT="Creaci&#xf3;n de la Rep&#xfa;blica Federal de Alemania (occidente)  y de la Rep&#xfa;blica Democr&#xe1;tica alemana (oriente)"/>
+<node CREATED="1424799837086" ID="ID_527910438" MODIFIED="1424799960497" TEXT="Francia, Inglaterra, Estados Unidos y Alemania crean la OTAN"/>
+<node CREATED="1424800020606" ID="ID_1488064789" MODIFIED="1424800034308" TEXT="Los sovi&#xe9;ticos y sus alidados crean el Pacto de Varsovia"/>
+<node CREATED="1424800076311" ID="ID_570475547" MODIFIED="1424800097678" TEXT="1961 - Tropas sovi&#xe9;ticas inician la construcci&#xf3;n del muro de Berl&#xed;n"/>
+<node CREATED="1424800098454" ID="ID_559362164" MODIFIED="1424800117731" TEXT="1989 - Cae el muro de Berl&#xed;n "/>
+</node>
+<node COLOR="#6699ff" CREATED="1424800285563" ID="ID_1158272055" MODIFIED="1424802797318" POSITION="right" TEXT="CHINA">
+<cloud COLOR="#33ff00"/>
+<node COLOR="#333300" CREATED="1424800376710" ID="ID_1539932892" MODIFIED="1424802476254" TEXT="1912 - Formaci&#xf3;n de la Rep&#xfa;blica China, dirigida por gobiernos nacionalistas">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424800307661" ID="ID_120337915" MODIFIED="1424802476254" TEXT="1949 - Mao Tse Tung derrota a los gobiernos nacionalistas">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424800337351" ID="ID_751750061" MODIFIED="1424802476253" TEXT="1949 - Constituci&#xf3;n de la Rep&#xfa;blica Popular China">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424800414983" ID="ID_1421239899" MODIFIED="1424802476253" TEXT="1949 - El gobierno nacionalista se retira a la isla de Formosa (Taiwan)">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424800508663" ID="ID_30595831" MODIFIED="1424802476253" TEXT="D&#xe9;cada de 1970 - La Rep&#xfa;blica Popular China es reconocida por la ONU">
+<edge COLOR="#ff6699"/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424800576140" ID="ID_929284290" MODIFIED="1424802820836" POSITION="left" TEXT="GUERRA DE COREA">
+<cloud COLOR="#cccc00"/>
+<node CREATED="1424800593116" ID="ID_661295939" MODIFIED="1424800607954" TEXT="Tropas sovi&#xe9;ticas y chinas invaden Corea del Sur"/>
+<node CREATED="1424800608966" ID="ID_779514241" MODIFIED="1424800632765" TEXT="Ej&#xe9;rcitos de estados unidos y otros pa&#xed;ses repelen la acci&#xf3;n"/>
+<node CREATED="1424800633791" ID="ID_1924927281" MODIFIED="1424800649548" TEXT="1953 - Firma del armisticio que termina la guerra"/>
+</node>
+<node COLOR="#6699ff" CREATED="1424800675250" ID="ID_1076196072" MODIFIED="1424802797317" POSITION="right" TEXT="MEDIO ORIENTE">
+<cloud COLOR="#33ff00"/>
+<node COLOR="#333300" CREATED="1424800685948" ID="ID_763815067" MODIFIED="1424802476253" TEXT="Despu&#xe9;s de la Segunda Guerra Mundial">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424800740378" ID="ID_697880566" MODIFIED="1424800796870" TEXT="Siria, L&#xed;bano Irak, Jordania , Palestina se independenizan de potencias occidentales"/>
+<node CREATED="1424800798014" ID="ID_1794343501" MODIFIED="1424800807527" TEXT="Formaci&#xf3;n de la Liga &#xc1;rabe"/>
+<node CREATED="1424800810159" ID="ID_66951201" MODIFIED="1424800827488" TEXT="1948 - La ONU establece el Estado Jud&#xed;o en tierras palestinas"/>
+<node CREATED="1424800851895" ID="ID_1261592517" MODIFIED="1424800867649" TEXT="M&#xfa;ltiples guerras entre judios y &#xe1;rabes">
+<node CREATED="1424800869619" ID="ID_804118972" MODIFIED="1424800893227" TEXT="Guerra de los seis d&#xed;as en 1967"/>
+<node CREATED="1424800893855" ID="ID_1134104910" MODIFIED="1424800902497" TEXT=" Yom Kipur de 1973"/>
+</node>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424800964075" ID="ID_398537627" MODIFIED="1424802820836" POSITION="left" TEXT="GUERRA DE VIETNAM">
+<cloud COLOR="#cccc00"/>
+<node COLOR="#333300" CREATED="1424800974346" ID="ID_905076434" MODIFIED="1424802404757" TEXT="1959 - Ej&#xe9;ricitos de Vietnam del norte (comunista) invaden Vietnam del sur">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801000711" ID="ID_1978059831" MODIFIED="1424802404756" TEXT="Estados Unidos apoya a Vietnam del Sur; chinos y sovi&#xe9;ticos a Vietnam del Norte">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801046406" ID="ID_1087214687" MODIFIED="1424802404756" TEXT="1975 - Finaliza la guerra con la derrota de los Estados Unidos">
+<edge COLOR="#ff6699"/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424801079244" ID="ID_977141845" MODIFIED="1424802797317" POSITION="right" TEXT="IR&#xc1;N">
+<cloud COLOR="#33ff00"/>
+<node COLOR="#333300" CREATED="1424801092596" ID="ID_1030488412" MODIFIED="1424802476253" TEXT="1979 - Triunfo de la revoluci&#xf3;n nacionalista y religiosa liderada por Ayatollah Ruhollah Khomeini ">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801160255" ID="ID_400228527" MODIFIED="1424802476252" TEXT="La revoluci&#xf3;n de Khomeini fue apoyada por los sovi&#xe9;ticos">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801188663" ID="ID_858891746" MODIFIED="1424802476252" TEXT="Se instaura un gobierno opuesto a los Estados Unidos. Nacionaliza el petr&#xf3;leo ">
+<edge COLOR="#ff6699"/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424801259349" ID="ID_1480035741" MODIFIED="1424802820835" POSITION="left" TEXT="AFGANIST&#xc1;N">
+<cloud COLOR="#cccc00"/>
+<node COLOR="#333300" CREATED="1424801282444" ID="ID_200260995" MODIFIED="1424802404756" TEXT="1979 - 1980 -  Tropas sovi&#xe9;ticas invaden y se instal&#xe1;n en territorio afgano">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801334287" ID="ID_232984945" MODIFIED="1424802404756" TEXT="1980 - 1985 - Operaciones militares a gran escala contra rebeldes afganos">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801355215" ID="ID_1027271659" MODIFIED="1424802404755" TEXT="1989 - Retiro de las tropas sovi&#xe9;ticas">
+<edge COLOR="#ff6699"/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424801385291" ID="ID_386461364" MODIFIED="1424802797317" POSITION="right" TEXT="DESCOLONIZACI&#xd3;N">
+<cloud COLOR="#33ff00"/>
+<node COLOR="#333300" CREATED="1424801403284" ID="ID_123750477" MODIFIED="1424802476252" TEXT="1945 - 1970 - Ola de independencias de colonias ubicadas en &#xc1;frica y Asia">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801462703" ID="ID_447089900" MODIFIED="1424802476252" TEXT="1955 - Conferencia de Bandung, Indonesia lideradas por Sukarnu (Indonesia) y Nehru (Egipto)">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801553599" ID="ID_364209632" MODIFIED="1424802476252" TEXT="1960 - Frmaci&#xf3;n del Movimiento de pa&#xed;ses No Alineados">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801634583" ID="ID_55330954" MODIFIED="1424802476251" TEXT="Tres vias de independencia">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424801644459" ID="ID_1914839367" MODIFIED="1424801688808" TEXT="Lucha armada - Argelia"/>
+<node CREATED="1424801649039" ID="ID_1090761006" MODIFIED="1424801696992" TEXT="Resistencia activa - India">
+<arrowlink DESTINATION="ID_1090761006" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_556514641" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+<node CREATED="1424801653999" ID="ID_1652543172" MODIFIED="1424801714016" TEXT="Negociaciones pol&#xed;ticas - &#xc1;frica "/>
+</node>
+</node>
+<node COLOR="#6699ff" CREATED="1424801736485" ID="ID_926717153" MODIFIED="1424802820835" POSITION="left" TEXT="UNI&#xd3;N EUROPEA">
+<cloud COLOR="#cccc00"/>
+<node COLOR="#333300" CREATED="1424801746211" ID="ID_1233616377" MODIFIED="1424802439069" TEXT="1957 - Alemania, B&#xe9;lgica, Francia, Italia, Luxemburgo y los Pa&#xed;ses Bajos crearon la Comunidad Econ&#xf3;mica Europea (CEE).">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801773143" ID="ID_1474964173" MODIFIED="1424802439069" TEXT="2002 - Creaci&#xf3;n del Euro como moneda &#xfa;nica y supresi&#xf3;n de controles fronterizos">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801849327" ID="ID_410531295" MODIFIED="1424802439069" TEXT="2009 - Tratado de Lisboa, carta pol&#xed;tica de la Uni&#xf3;n">
+<edge COLOR="#ff6699"/>
+</node>
+<node COLOR="#333300" CREATED="1424801879152" ID="ID_336421866" MODIFIED="1424802439068" TEXT="Organizaci&#xf3;n de la Uni&#xf3;n Europea">
+<edge COLOR="#ff6699"/>
+<node CREATED="1424801932869" ID="ID_1671372088" MODIFIED="1424801945397" TEXT="Parlamento Europeo - &#xd3;rgano legislativo"/>
+<node CREATED="1424801945617" FOLDED="true" ID="ID_1426056773" MODIFIED="1424801995092" TEXT="Comisi&#xf3;n Europea - &#xd3;rgano ejecutivo">
+<node CREATED="1424801974677" ID="ID_1449258552" MODIFIED="1424801984485" TEXT="Presidente de la Comisi&#xf3;n"/>
+</node>
+<node CREATED="1424801996744" ID="ID_337879071" MODIFIED="1424802018892" TEXT="Uni&#xf3;n Europea - integrada por 28 pa&#xed;ses"/>
+</node>
+</node>
+</node>
+</map>
