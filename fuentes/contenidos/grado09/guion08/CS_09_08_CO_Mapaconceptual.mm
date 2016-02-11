fdbc2d57538a8caf0410a9196642875b51e07596
Jeysson Moreno <yeyemoto@hotmail.com>
2016-02-11 10:38:03 -0500

    11/02 - 1
    CS_09_08_CO y CS_09_09_CO

---
+++ fuentes/contenidos/grado09/guion08/CS_09_08_CO_Mapaconceptual.mm
@@ -0,0 +1,50 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453646145297" ID="ID_1827419428" MODIFIED="1453740323789" TEXT="Am&#xe9;rica Latina desde la segunda mitad del siglo XX">
+<node CREATED="1453740192478" ID="ID_71818943" MODIFIED="1453740317056" POSITION="right" TEXT="afront&#xf3; ">
+<node CREATED="1453646171294" ID="ID_1253933897" MODIFIED="1453742514021" STYLE="bubble" TEXT="El fin de la Guerra Fr&#xed;a">
+<node CREATED="1453646980374" ID="ID_845142710" MODIFIED="1453742895595" STYLE="fork" TEXT="impuls&#xf3;">
+<node CREATED="1453647250607" ID="ID_426446085" MODIFIED="1453742925520" STYLE="bubble" TEXT="el libre comercio"/>
+<node CREATED="1453647330790" ID="ID_577291132" MODIFIED="1453742910713" STYLE="bubble" TEXT="las privatizaciones"/>
+</node>
+<node CREATED="1453647226972" ID="ID_949449431" MODIFIED="1453742843745" STYLE="fork" TEXT="estimul&#xf3;">
+<node CREATED="1453647901546" ID="ID_472255798" MODIFIED="1453742878842" STYLE="bubble" TEXT="los bloques de intercambio regional"/>
+<node CREATED="1453647731915" ID="ID_305329964" MODIFIED="1453742860645" STYLE="bubble" TEXT="la integraci&#xf3;n econ&#xf3;mica"/>
+</node>
+<node CREATED="1453646967253" ID="ID_313494291" MODIFIED="1453742695791" STYLE="fork" TEXT="favoreci&#xf3;">
+<node CREATED="1453647167391" ID="ID_936494672" MODIFIED="1453742795356" STYLE="bubble" TEXT="nuevos gobiernos de distintas ideolog&#xed;as"/>
+<node CREATED="1453646985851" ID="ID_569064629" MODIFIED="1453742734871" STYLE="bubble" TEXT="el regreso a la democracia electoral"/>
+</node>
+</node>
+<node CREATED="1453646168226" ID="ID_190590039" MODIFIED="1453740377778" STYLE="bubble" TEXT="La Guerra Fr&#xed;a">
+<node CREATED="1453646255437" ID="ID_1214455756" MODIFIED="1453646285139" STYLE="fork" TEXT="provoc&#xf3;">
+<node CREATED="1453646380500" ID="ID_454187600" MODIFIED="1453646505817" STYLE="bubble" TEXT="resistencia">
+<node CREATED="1453646651824" ID="ID_882916916" MODIFIED="1453741221293" STYLE="fork" TEXT="manifiesta en">
+<node CREATED="1453646691020" ID="ID_831212288" MODIFIED="1453742458382" STYLE="bubble" TEXT="el triunfo de gobiernos socialistas">
+<node CREATED="1453646726389" ID="ID_1139935500" MODIFIED="1453646775927" STYLE="fork" TEXT="en">
+<node CREATED="1453646779283" ID="ID_1756422734" MODIFIED="1453646801318" STYLE="bubble" TEXT="Cuba, Chile, Nicaragua"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1453646494581" ID="ID_367847353" MODIFIED="1453647164119" STYLE="bubble" TEXT="dictaduras militares">
+<node CREATED="1453646646753" ID="ID_1300769615" MODIFIED="1453646836348" STYLE="fork" TEXT="que buscaron">
+<node CREATED="1453646847095" ID="ID_1038460095" MODIFIED="1453741086948" STYLE="bubble" TEXT="* contener el socialismo   * aplicar el neoliberalismo"/>
+</node>
+</node>
+</node>
+<node CREATED="1453646246306" ID="ID_1178319962" MODIFIED="1453646285139" STYLE="fork" TEXT="signific&#xf3;">
+<node CREATED="1453646290108" ID="ID_68846888" MODIFIED="1453815235035" STYLE="bubble" TEXT="la intervenci&#xf3;n de EE. UU. en Am&#xe9;rica Latina">
+<node CREATED="1453646321266" ID="ID_601036648" MODIFIED="1453646331579" STYLE="fork" TEXT="para">
+<node CREATED="1453646340897" ID="ID_619663617" MODIFIED="1453743008011" STYLE="bubble" TEXT="* garantizar seguridad a los gobiernos">
+<node CREATED="1453646428300" ID="ID_1741560061" MODIFIED="1453646446816" STYLE="fork" TEXT="frente a ">
+<node CREATED="1453646466870" ID="ID_1747928685" MODIFIED="1453742444101" STYLE="bubble" TEXT=" la penetraci&#xf3;n del comunismo"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ fuentes/contenidos/grado09/guion09/CS_09_09_CO_Mapa conceptual.mm
@@ -0,0 +1,46 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453648736699" ID="ID_50312649" MODIFIED="1453813721875" TEXT="Colombia desde la segunda mitad del siglo XX">
+<node CREATED="1453648805887" ID="ID_412391339" MODIFIED="1453650363999" POSITION="right" STYLE="bubble" TEXT="Finales del siglo XX y  nuevo milenio">
+<node CREATED="1453649724706" ID="ID_1685884296" MODIFIED="1453816731920" STYLE="fork" TEXT="gobiernos">
+<node CREATED="1453649745558" ID="ID_1400772721" MODIFIED="1453819884280" STYLE="bubble" TEXT="Virgilio Barco - C&#xe9;sar Gaviria - Ernesto Samper - Andr&#xe9;s Pastrana - &#xc1;lvaro Uribe - Juan Manuel Santos"/>
+</node>
+<node CREATED="1453649722778" ID="ID_787788692" MODIFIED="1453816791370" STYLE="fork" TEXT="hechos">
+<node CREATED="1453650321971" ID="ID_1032662541" MODIFIED="1453817071315" STYLE="bubble" TEXT="el surgimiento del paramilitarismo, las masacres, los desplazamientos forzados"/>
+<node CREATED="1453649863127" ID="ID_25635605" MODIFIED="1453816567195" STYLE="bubble" TEXT="la iniciativa ciudadana constituyente">
+<node CREATED="1453649913974" ID="ID_1048945347" MODIFIED="1453649952264" STYLE="fork" TEXT="que logr&#xf3;">
+<node CREATED="1453649955140" ID="ID_911794899" MODIFIED="1453816848271" STYLE="bubble" TEXT="la nueva Constituci&#xf3;n Pol&#xed;tica en 1991 "/>
+</node>
+</node>
+<node CREATED="1453650572874" ID="ID_654255735" MODIFIED="1453816555865" STYLE="bubble" TEXT="los procesos de paz"/>
+<node CREATED="1453649853338" ID="ID_1982398396" MODIFIED="1453817014571" STYLE="bubble" TEXT="la apertura econ&#xf3;mica, la crisis econ&#xf3;mica"/>
+<node CREATED="1453649822672" ID="ID_879850117" MODIFIED="1453817046760" STYLE="bubble" TEXT="el auge del  narcotr&#xe1;fico, los magnicidios"/>
+</node>
+</node>
+<node CREATED="1453648768429" ID="ID_1093152218" MODIFIED="1453823645984" POSITION="right" STYLE="bubble" TEXT="Despu&#xe9;s del Frente Nacional (1974&#x2013;1986)">
+<node CREATED="1453649521150" ID="ID_496342394" MODIFIED="1453816745972" STYLE="fork" TEXT="gobiernos">
+<node CREATED="1453649553070" ID="ID_1778094135" MODIFIED="1453650474856" STYLE="bubble" TEXT="Alfonso L&#xf3;pez, Julio C&#xe9;sar Turbay , Belisario Betancur"/>
+</node>
+<node CREATED="1453649331576" ID="ID_1888772121" MODIFIED="1453816680615" STYLE="fork" TEXT="hechos">
+<node CREATED="1453649452661" ID="ID_1299651755" MODIFIED="1453816431281" STYLE="bubble" TEXT="el surgimiento del narc&#xf3;tr&#xe1;fico"/>
+<node CREATED="1453649443651" ID="ID_1428359848" MODIFIED="1453816415926" STYLE="bubble" TEXT="la represi&#xf3;n pol&#xed;tica"/>
+<node CREATED="1453649431664" ID="ID_915315317" MODIFIED="1453816399007" STYLE="bubble" TEXT="el recrudecimiento de la violencia"/>
+</node>
+</node>
+<node CREATED="1453648752529" ID="ID_943007888" MODIFIED="1453823473930" POSITION="right" STYLE="bubble" TEXT=" Frente Nacional (1958-1974)">
+<node CREATED="1453649246208" ID="ID_1522458341" MODIFIED="1453815274415" STYLE="fork" TEXT="resultados">
+<node CREATED="1453649264219" ID="ID_1962240633" MODIFIED="1453815978796" STYLE="bubble" TEXT="la exclusi&#xf3;n de otras fuerzas pol&#xed;ticas"/>
+<node CREATED="1453649273569" ID="ID_254631741" MODIFIED="1453823760016" STYLE="bubble" TEXT="el surgimiento de las guerrillas (FARC - ELN)"/>
+</node>
+<node CREATED="1453648988313" ID="ID_386544816" MODIFIED="1453816764831" STYLE="fork" TEXT="gobiernos">
+<node CREATED="1453649121836" ID="ID_1941937195" MODIFIED="1453819260727" STYLE="bubble" TEXT="Alberto Lleras  - Guillermo Le&#xf3;n Valencia - Carlos Lleras  - Misael Pastrana"/>
+</node>
+<node CREATED="1453648982706" HGAP="78" ID="ID_108870683" MODIFIED="1453815379270" STYLE="fork" TEXT="busc&#xf3;" VSHIFT="38">
+<node CREATED="1453649039038" ID="ID_783144659" MODIFIED="1453815966248" STYLE="bubble" TEXT="frenar la violencia bipartidista"/>
+</node>
+<node CREATED="1453648937507" HGAP="29" ID="ID_795356605" MODIFIED="1453814675945" STYLE="fork" TEXT="fue" VSHIFT="1">
+<node CREATED="1453648998082" HGAP="79" ID="ID_1019447144" MODIFIED="1453823535395" STYLE="bubble" TEXT="un pacto entre los partidos liberal y conservador" VSHIFT="10"/>
+</node>
+</node>
+</node>
+</map>
