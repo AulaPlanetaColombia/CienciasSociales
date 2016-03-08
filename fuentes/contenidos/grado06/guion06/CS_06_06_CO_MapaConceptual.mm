93b31556d9b566e006bad015ed72091142b79a43
Marcela Guevara B <marsguev@hotmail.com>
2016-03-08 10:01:41 -0500

    06_05 y 06_06
    y Guía didáctica 12

---
+++ fuentes/contenidos/grado06/guion06/CS_06_06_CO_MapaConceptual.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453758611082" ID="ID_182650497" MODIFIED="1453758907746" TEXT="Colombia precolombina 20 000 a.C. - 1499">
+<node CREATED="1453758670863" ID="ID_1977882384" MODIFIED="1453760646835" POSITION="right" STYLE="bubble" TEXT="Otras culturas">
+<node CREATED="1453760643852" ID="ID_962575897" MODIFIED="1453760827146" TEXT="Tierradentro: * Cauca * centro ceremonial * hipogeos y estatuaria en piedra">
+<arrowlink DESTINATION="ID_1135343107" ENDARROW="Default" ENDINCLINATION="47;0;" ID="Arrow_ID_1631376207" STARTARROW="None" STARTINCLINATION="47;0;"/>
+</node>
+<node CREATED="1453760649114" ID="ID_1836366652" MODIFIED="1453760827092" TEXT="San Agust&#xed;n: * sur del Huila * centro ceremonial * tumbas y esculturas en piedra"/>
+</node>
+<node CREATED="1453758860681" ID="ID_1265181123" MODIFIED="1453759016508" POSITION="left" STYLE="bubble" TEXT="Etapas arqueol&#xf3;gicas">
+<node CREATED="1453759003124" ID="ID_842724011" MODIFIED="1453759089702" TEXT="Paleoindio: * 20 000 a.C. - 7 000 a.C. * hordas de cazadores y recolectores n&#xf3;madas"/>
+<node CREATED="1453759006548" ID="ID_1441955639" MODIFIED="1453759349958" TEXT="Arcaico: * 3 000 a.C. - 1 200 a.C. * hordas de pescadores y recolectores * sedentarismo estacional"/>
+<node CREATED="1453759011870" ID="ID_1976406965" MODIFIED="1453759319999" TEXT="Formativo: * 1 200 a.C. - 1499 * sociedades agr&#xed;colas * sedentarismo * divisi&#xf3;n del trabajo * cacic&#xe1;zgos"/>
+</node>
+<node CREATED="1453758912335" ID="ID_1126060570" MODIFIED="1453759430259" POSITION="right" STYLE="bubble" TEXT="Familias linguisticas">
+<node CREATED="1453759421221" ID="ID_973217431" MODIFIED="1453759466692" TEXT="Arawak">
+<node CREATED="1453760598103" ID="ID_1135343107" MODIFIED="1453760827145" TEXT="cultura Way&#xfa;u: * La Guajira *cazadores, pescadores">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1135343107" ENDARROW="Default" ENDINCLINATION="47;0;" ID="Arrow_ID_1631376207" SOURCE="ID_962575897" STARTARROW="None" STARTINCLINATION="47;0;"/>
+</node>
+</node>
+<node CREATED="1453759430261" ID="ID_1368421979" MODIFIED="1453759785159" TEXT="Caribe">
+<node CREATED="1453759781072" ID="ID_1952451124" MODIFIED="1453760544465" TEXT="cultura Calima: *Valle del Cauca *orfebrer&#xed;a,cer&#xe1;mica"/>
+<node CREATED="1453759785161" ID="ID_624734051" MODIFIED="1453760505520" TEXT="cultura Quimbaya: *Quind&#xed;o, Risaralda, Caldas, Valle * orfebrer&#xed;a, cer&#xe1;mica"/>
+<node CREATED="1453759790305" ID="ID_1740805983" MODIFIED="1453759886510" TEXT="Cultura Sin&#xfa;: *departamento de C&#xf3;rdoba * obras hidr&#xe1;ulicas, orfebrer&#xed;a, cer&#xe1;mica * cacic&#xe1;zgos"/>
+</node>
+<node CREATED="1453759434205" ID="ID_1163821501" MODIFIED="1453759451238" TEXT="Chibcha">
+<node CREATED="1453759477993" ID="ID_1775594018" MODIFIED="1453759744109" TEXT="cultura Muisca: * Altiplano cundiboyacense * confederaci&#xf3;n de aldeas * orfebrer&#xed;a, cer&#xe1;mica, tejidos"/>
+<node CREATED="1453759481549" ID="ID_612479522" MODIFIED="1453759763781" TEXT="cultura Tairona: *Sierra Nevada de Santa Marta * construcciones en piedra * cacic&#xe1;zgos * orfebrer&#xed;a *tejidos *cer&#xe1;mica"/>
+</node>
+</node>
+</node>
+</map>
