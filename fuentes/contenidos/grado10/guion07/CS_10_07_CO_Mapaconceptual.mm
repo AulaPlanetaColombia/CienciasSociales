57d51d26557831044181ad1ddff1c17d4f04d54a
Marcela Guevara B <marsguev@hotmail.com>
2016-05-18 16:15:39 -0500

    Para publicar
    CS_10_07_CO

---
+++ fuentes/contenidos/grado10/guion07/CS_10_07_CO_Mapaconceptual.mm
@@ -0,0 +1,46 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1463491301703" ID="ID_578243738" MODIFIED="1463491699094" TEXT="La sociedad y la cultura">
+<node CREATED="1463491311761" ID="ID_807947532" MODIFIED="1463491544191" POSITION="right" STYLE="fork" TEXT="la convivencia humana da lugar a ">
+<node CREATED="1463491575241" ID="ID_9360590" MODIFIED="1463587688800" STYLE="bubble" TEXT="diversidad cultural">
+<node CREATED="1463494026803" ID="ID_1078860130" MODIFIED="1463496155157" STYLE="fork" TEXT="est&#xe1; presente en">
+<node CREATED="1463496158821" ID="ID_1669828075" MODIFIED="1463496184774" STYLE="bubble" TEXT="Colombia">
+<node CREATED="1463496186632" ID="ID_690460430" MODIFIED="1463496233445" STYLE="fork" TEXT="que se reconoce como">
+<node CREATED="1463496235940" ID="ID_213692941" MODIFIED="1463496682179" STYLE="bubble" TEXT="naci&#xf3;n diversa, pluri&#xe9;tnica y multicultural">
+<node CREATED="1463496528173" ID="ID_419810291" MODIFIED="1463496542977" STYLE="fork" TEXT="con presencia de">
+<node CREATED="1463496544896" ID="ID_1247738840" MODIFIED="1463587734077" STYLE="bubble" TEXT="ind&#xed;genas - palenqueros - raizales - rom - mestizos "/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1463492047362" ID="ID_517862078" MODIFIED="1463492067019" STYLE="fork" TEXT="que se expresa en">
+<node CREATED="1463492070250" ID="ID_959107383" MODIFIED="1463587745115" STYLE="bubble" TEXT="costumbres - pr&#xe1;cticas - lenguajes - tradiciones - ideas - valores - creencias - conocimientos">
+<node CREATED="1463492167170" ID="ID_50108007" MODIFIED="1463492303136" STYLE="fork" TEXT="dan origen a ">
+<node CREATED="1463492363118" FOLDED="true" ID="ID_1717067576" MODIFIED="1463587774350" STYLE="bubble" TEXT="conflictos: econ&#xf3;micos - pol&#xed;ticos - territoriales - religiosos - &#xe9;tnicos - de g&#xe9;nero">
+<node CREATED="1463493629735" ID="ID_1149310911" MODIFIED="1463493787287" STYLE="fork" TEXT="soluciones pac&#xed;ficas mediante">
+<node CREATED="1463493662732" ID="ID_1841424100" MODIFIED="1463493746865" STYLE="bubble" TEXT="- declaraciones - tratados - constituciones - acuerdos"/>
+</node>
+</node>
+<node CREATED="1463492305011" ID="ID_1812700554" MODIFIED="1463493395027" STYLE="bubble" TEXT="pluriculturalidad: herencias m&#xfa;ltiples - reconocimiento y valoraci&#xf3;n de otras culturas - pluralismo - democracia"/>
+<node CREATED="1463492923010" ID="ID_1039677828" MODIFIED="1463587782532" STYLE="bubble" TEXT="interculturalidad: relaci&#xf3;n entre culturas - pac&#xed;fica o conflictiva - incluyente o excluyente "/>
+<node CREATED="1463492356130" ID="ID_1145466522" MODIFIED="1463495203638" STYLE="bubble" TEXT="cultura de masas">
+<node CREATED="1463499577973" ID="ID_1435612173" MODIFIED="1463499591236" STYLE="fork" TEXT="caracterizada por">
+<node CREATED="1463499594261" ID="ID_1636048817" MODIFIED="1463587796332" STYLE="bubble" TEXT="homogenizaci&#xf3;n de la cultura - industria cultural - consumo masivo - globalizaci&#xf3;n econ&#xf3;mica"/>
+</node>
+</node>
+<node CREATED="1463492348731" ID="ID_23552654" MODIFIED="1463587812703" STYLE="bubble" TEXT="multiculturalidad: interacci&#xf3;n de culturas en el mismo territorio"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1463491578930" ID="ID_1750173180" MODIFIED="1463587699279" STYLE="bubble" TEXT="grupos sociales">
+<node CREATED="1463491827925" ID="ID_1416403195" MODIFIED="1463491844270" STYLE="fork" TEXT="con">
+<node CREATED="1463491848285" ID="ID_1169074114" MODIFIED="1463587816517" STYLE="bubble" TEXT="modelos de vida: tradicional - moderno - posmoderno"/>
+<node CREATED="1463491859746" ID="ID_1168685665" MODIFIED="1463587820211" STYLE="bubble" TEXT="estructuras: est&#xe1;ticas - din&#xe1;micas"/>
+<node CREATED="1463491866498" ID="ID_1792164770" MODIFIED="1463587841686" STYLE="bubble" TEXT="sistemas de organizaci&#xf3;n: econ&#xf3;micos - pol&#xed;ticos - jur&#xed;dicos - culturales - &#xe9;ticos"/>
+</node>
+</node>
+</node>
+</node>
+</map>
